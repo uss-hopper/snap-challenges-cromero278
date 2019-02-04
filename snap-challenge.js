8411682c6f2d771e9bd69185529e6c4f4bb09f8c
Cassandra Romero <romero.cassandra1@gmail.com>
2019-02-04 09:13:20 -0700

    result (not correct but attempted)

--- snap-challenge.js
+++ snap-challenge.js
@@ -1,4 +1,14 @@
-const users = [
+
+let string = "Fate protects fools, little children and ships named Enterprise. You're going to be an interesting companion, Mr. Data. Some days you get the bear, and some days the bear gets you. Maybe if we felt any human loss as keenly as we feel one of those close to us, human history would be far less bloody. Why don't we just give everybody a promotion and call it a night - 'Commander'? Well, that's certainly good to know. Mr. Worf, you do remember how to fire phasers? A lot of things can change in twelve years, Admiral.";
+
+
+let arr = string.split(" ");
+	arr = arr.filter((arr) => arr.startsWith("f"));
+
+
+console.log(arr);
+
+/*const users = [
 	{
 		"userId": 1,
 		"id": 1,
@@ -34,3 +44,5 @@ const result = users.filter(function(index){
 	return ([index.userId = 1, 2, 3, 4]);
 });
 console.log(result);
+
+*/
