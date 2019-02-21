99c413dc2588cd0fb6c64c220dbcab2959591e56
Cassandra Romero <romero.cassandra1@gmail.com>
2019-02-21 08:21:34 -0700

    snap challenge 2/21

--- snap-challenge.js
+++ snap-challenge.js
@@ -1,4 +1,11 @@
-const array = [1,2,3,5,8,13,21,34];
+const string = "I'll alert the crew. We finished our first sensor sweep of the neutral zone. Maybe if we felt any human loss as keenly as we feel one of those close to us, human history would be far less bloody.";
+
+let newString = [...string].reduce((a, b)=>b+a);
+
+console.log(newString);
+
+
+/*const array = [1,2,3,5,8,13,21,34];
 
 let total = array.reduce((a, b) => a * b);
 
